8bbd1ade1b605bc7c71c999b8297e426aa6a5af8
gouthamb9599 <gouthamrahul1095@gmail.com>
2020-10-07 12:36:01 +0530

    timer

--- src/components/codeGenerator.js
+++ src/components/codeGenerator.js
@@ -1,15 +1,57 @@
 import React from "react";
 import { Button } from "antd";
+const cryptoRandomString = require("crypto-random-string");
+import Timer from 'react-timer-wrapper';
+
+
 
 function CodeGenerator(props) {
+  const [timer,openTimer]=useState(false);
+  const generateCode = () => {
+   console.log(cryptoRandomString({ length: 6}))
+   openTimer(!timer);
+  };
+  const onTimerStart=({duration, progress, time})=> {
+
+  }
+
+  const onTimerStop=({duration, progress, time})=> {
+
+  }
+
+  const onTimerTimeUpdate=({duration, progress, time})=> {
+
+  }
+
+  const onTimerFinish=({duration, progress, time})=> {
+
+  }
   return (
-    <div style={{ borderStyle: "dashed" }}>
+    <div style={{ borderStyle: "dashed",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    paddingTop: "20px",
+    paddingBottom: "20px"}}>
       <p>
-        Please Click On generate code button to recieve it from the customer and
+        Please Click on generate code button to recieve it from the customer and
         enter the code to change the subscriber number
       </p>
-      <Button>Generate Code</Button>
+      <Button  style={{
+          marginLeft: "50px",
+          marginRight: "50px"
+        }}
+        onClick={generateCode}>Generate Code</Button>
+        {timer ?<div> 
+          <Timer
+        active={timer}
+        time={20000}
+        onFinish={this.onTimerFinish}
+        onStart={this.onTimerStart}
+        onStop={this.onTimerStop}
+        onTimeUpdate={this.onTimerTimeUpdate}
+      /></div>:<></>}
     </div>
   );
 }
 export default CodeGenerator;
+
